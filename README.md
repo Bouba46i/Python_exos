af670a256006a1282aafaef42a284fbc1ed892fa
bbouvy <bouvb.pro@gmail.com>
2025-10-26 22:07:38 +0100

    📈 exo 2 with  fully functional GOL

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 Test pour Nalios (cc)
 
+pour executer l'exo 2, numpy est requis
+> pip3 install numpy
+
 ___
 
 GitHub commits explanation :
--- Test/exo 2.py
+++ Test/exo 2.py
@@ -1,56 +1,61 @@
 #TODO :
 # implementer le game of life
-# 	les regles -> implementer les cas cas tricky de matrice
-#	lire la matrice (tableau a deux dim n x m)
-#	pour chaque cell -> réécrire son destin (c'est dramatique oui)
-#	lancer le GOL 5 fois sur la matrice
-#	
+#   matrice n x m
+#   j'assume que n > 0 & m > 0	
 #
 # bonus
 #	creer un fichier html avec le resultat
 #		se décider du format du resultat
 #
+import copy
+import numpy as np
 
 
-GOL_matrix = [
-	[0, 1, 0, 1, 1]
-	[0, 0, 1, 1, 0]
-	[0, 1, 0, 0, 1]
+GOL_matrix = np.array([
+	[0, 1, 0, 1, 1],
+	[0, 0, 1, 1, 0],
+	[0, 1, 0, 0, 1],
 	[0, 1, 0, 1, 0]
-]
+])
 
 
 
-def neighbors_count(matrix):
-    # lire son voisinages + compter les cellules vivantes
-    return 0
+def neighbors_count(matrix, i, j):
+    rows, cols = matrix.shape
+    i_min = i - 1 if i - 1 >= 0 else 0 
+    i_max = i + 2 if i + 2 <= rows else rows 
+    j_min = j - 1 if j - 1 >= 0 else 0
+    j_max = j + 2 if j + 2 <= cols else cols 
+    
+    return np.sum(matrix[i_min:i_max, j_min:j_max]) - matrix[i, j]
 
-
-def is_cell_will_live(is_living, neig_count):
+def will_cell_live(is_living, neig_count):
     if(neig_count == 3): return True
     if(is_living and neig_count == 2): return True
     return False
 
 def gol_iteration(matrix):
-    new_matrix = [[],[]]
+    new_matrix = copy.deepcopy(matrix)
     
-	# pour chaque cell, vivante ou non
-    # 	compter le nombre de cellules vivantes autour d'elle
-    # 	neig_count = neighbors_count(matrix)
-    # 	cell_updated = is_cell_will_live(is_living, neig_count)
-
+    for (i, j), value in np.ndenumerate(matrix):
+        neig_count = neighbors_count(matrix, i, j)
+        new_matrix[i, j] = will_cell_live(value, neig_count)
+	
     return new_matrix
 
 # bonus pour apres
 # fonction pour creer fichier html
 
 def game_of_life(matrix):
-    # test cas tricky de la matrice
 
     for i in range(0, 5):
         matrix = gol_iteration(matrix)
-        
+    
     # appeler fonction pour creer fichier html
-    return 
+    # print_hmtl(matrix)
+    
+    return None
+
+
 
 game_of_life(GOL_matrix)
